3f53c0510bb3eb0f6478423cb2c1016fa2f65859
noor-ullah-khan <mr.noor7543@gmail.com>
2024-10-11 22:59:30 +0500

    add Spherum

---
+++ Spherum.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Spherum.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Spherum.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Spherum.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
