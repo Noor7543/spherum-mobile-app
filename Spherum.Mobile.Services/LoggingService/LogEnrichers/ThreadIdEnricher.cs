21fe2b38004d65399a23f2b61a8a5eef081bd1af
noor-ullah-khan <mr.noor7543@gmail.com>
2024-10-17 14:43:42 +0500

    add logger

---
+++ Spherum.Mobile.Services/LoggingService/LogEnrichers/ThreadIdEnricher.cs
@@ -0,0 +1,21 @@
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Spherum.Mobile.Services.LoggingService.LogEnrichers;
+
+/// <summary>
+/// Enriches log events with the current thread's ID.
+/// </summary>
+public sealed class ThreadIdEnricher : ILogEventEnricher
+{
+    /// <summary>
+    /// Enriches the log event with the current thread's ID.
+    /// </summary>
+    /// <param name="logEvent">A log event.</param>
+    /// <param name="propertyFactory">Creates log event properties from regular .NET objects, applying policies as
+    /// required.</param>
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Thread", Environment.CurrentManagedThreadId));
+    }
+}
---
+++ Spherum.Mobile/MauiProgram.Logger.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Spherum.Mobile.Services.LoggingService.Extensions;
+using LoggerExtensions = Spherum.Mobile.Services.LoggingService.Extensions.LoggerExtensions;
+
+namespace Spherum.Mobile;
+
+public static partial class MauiProgram
+{
+    static void ConfigureAndAddLogger(this IServiceCollection services)
+    {
+        var config = LoggerExtensions.DefaultConfig;
+              // We can add AppCenter sink here
+        config.WriteTo.
+        // Add file sink, will output to the file specified in the path
+        File(LoggerExtensions.DefaultExpressionTemplate,
+             Path.Combine(FileSystem.AppDataDirectory, "Logs/logs.txt"), LogEventLevel.Information,
+             rollingInterval: RollingInterval.Day);
+        var logger = LoggerExtensions.CreateLogger(config);
+        services.AddLogging(options =>
+        {
+            options.ClearProviders();
+            options.AddLogger(logger);
+        });
+        services.AddSingleton(logger); // add the logger as a singleton for DI
+    }
+}
